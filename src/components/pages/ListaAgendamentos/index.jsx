1bcfeb635f0c64500abf49cf63476765b614ecd2
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-06-02 10:06:56 -0300

    api de listagem de agendamentos

--- src/components/pages/ListaAgendamentos/index.jsx
+++ src/components/pages/ListaAgendamentos/index.jsx
@@ -1,52 +1,80 @@
 import { Tabela } from './styles'
-
+import { useEffect, useState } from 'react'
+import api from '../../../lib/api'
 
 export function ListaAgendamentos() {
 
+  const [listAgendamentos, setListAgendamentos] = useState([]);
+
+  useEffect(() => {
+    async function getAgendamentos() {
+      const response = await api.get('')
+        .then(response => {
+          setListAgendamentos(response.data)
+          console.log(response.data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    };
+    getAgendamentos();
+  }, []);
+
   return (
+    <div>
+      ok
+    </div>
 
-    <Tabela>
-      <table>
-        <tr>
-          <th>Id</th>
-          <th>Nome</th>
-          <th>Campus</th>
-          <th>Setor</th>
-          <th>Categoria</th>
-          <th>Agendamento</th>
-          <th>Status</th>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Mattheus Adhonnay</td>
-          <td>Petrolina</td>
-          <td>SRCA</td>
-          <td>Matrícula</td>
-          <td>2022-06-15 15:00</td>
-          <td>Não atendido</td>
-        </tr>
+    // <Tabela>
+    //   <table>
 
-        <tr>
-          <td>2</td>
-          <td>Sebastião Casca de pinha</td>
-          <td>Paulo Afonso</td>
-          <td>PROAE</td>
-          <td>Entrega de documentos</td>
-          <td>2022-08-23 15:30</td>
-          <td>Atendido</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>Ledarno Chuponis</td>
-          <td>Juazeiro</td>
-          <td>SRCA</td>
-          <td>Entrega de diploma</td>
-          <td>2022-06-05 12:00</td>
-          <td>Não atendido</td>
-        </tr>
-        </table>
+    //     <tr>
+    //       <th>Id</th>
+    //       <th>Nome</th>
+    //       <th>{}</th>
+    //       <th>Setor</th>
+    //       <th>Categoria</th>
+    //       <th>Agendamento</th>
+    //       <th>Status</th>
+    //     </tr>
+    //     <tr>
+    //       <td>1</td>
+    //       <td>Mattheus Adhonnay</td>
+    //       <td>Petrolina</td>
+    //       <td>SRCA</td>
+    //       <td>Matrícula</td>
+    //       <td>2022-06-15 15:00</td>
+    //       <td>Não atendido</td>
+    //     </tr>
 
-        
-    </Tabela >
+    //     <tr>
+    //       <td>2</td>
+    //       <td>Sebastião Casca de pinha</td>
+    //       <td>Paulo Afonso</td>
+    //       <td>PROAE</td>
+    //       <td>Entrega de documentos</td>
+    //       <td>2022-08-23 15:30</td>
+    //       <td>Atendido</td>
+    //     </tr>
+    //     <tr>
+    //       <td>3</td>
+    //       <td>Ledarno Chuponis</td>
+    //       <td>Juazeiro</td>
+    //       <td>SRCA</td>
+    //       <td>Entrega de diploma</td>
+    //       <td>2022-06-05 12:00</td>
+    //       <td>Não atendido</td>
+    //     </tr>
+    //   </table>
+
+
+    // </Tabela >
   )
 }
+
+
+
+
+
+
+
