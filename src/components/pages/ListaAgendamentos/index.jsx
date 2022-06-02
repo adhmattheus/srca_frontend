21e96441b0a766a24e48fbeddc39e58f06520fe4
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-06-02 12:21:48 -0300

    api get

--- src/components/pages/ListaAgendamentos/index.jsx
+++ src/components/pages/ListaAgendamentos/index.jsx
@@ -8,10 +8,11 @@ export function ListaAgendamentos() {
 
   useEffect(() => {
     async function getAgendamentos() {
-      const response = await api.get('')
+      const response = await api.get('https://jsonplaceholder.typicode.com/users')
         .then(response => {
           setListAgendamentos(response.data)
           console.log(response.data)
+
         })
         .catch(err => {
           console.log(err)
@@ -20,18 +21,24 @@ export function ListaAgendamentos() {
     getAgendamentos();
   }, []);
 
-  return (
-    <div>
-      ok
-    </div>
+  // const  = listAgendamentos.map((agendamento) => {
+  //   return
+  //   <div>
+  //     <p>{agendamento.id}</p>
+  //     <p>{user.name}</p>
+  //   </div>
+  // })
 
-    // <Tabela>
+  return (
+<p>ok</p>
+    
+    //   <Tabela>
     //   <table>
 
     //     <tr>
     //       <th>Id</th>
     //       <th>Nome</th>
-    //       <th>{}</th>
+    //       <th>Campus</th>
     //       <th>Setor</th>
     //       <th>Categoria</th>
     //       <th>Agendamento</th>
@@ -46,29 +53,11 @@ export function ListaAgendamentos() {
     //       <td>2022-06-15 15:00</td>
     //       <td>Não atendido</td>
     //     </tr>
-
-    //     <tr>
-    //       <td>2</td>
-    //       <td>Sebastião Casca de pinha</td>
-    //       <td>Paulo Afonso</td>
-    //       <td>PROAE</td>
-    //       <td>Entrega de documentos</td>
-    //       <td>2022-08-23 15:30</td>
-    //       <td>Atendido</td>
-    //     </tr>
-    //     <tr>
-    //       <td>3</td>
-    //       <td>Ledarno Chuponis</td>
-    //       <td>Juazeiro</td>
-    //       <td>SRCA</td>
-    //       <td>Entrega de diploma</td>
-    //       <td>2022-06-05 12:00</td>
-    //       <td>Não atendido</td>
-    //     </tr>
     //   </table>
+    // </Tabela>
 
 
-    // </Tabela >
+
   )
 }
 
