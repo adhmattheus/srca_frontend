2fe87ea3fb8e137dcb165309ff35be7936204aab
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-07-27 13:33:38 -0300

    cadastro de alunos

--- src/pages/CadastroPage/index.jsx
+++ src/pages/CadastroPage/index.jsx
@@ -1,22 +1,67 @@
 import { Container } from "./styles";
 import { useNavigate } from "react-router-dom";
-import { FiLogIn, FiMail } from "react-icons/fi";
+import { FiArrowLeft } from "react-icons/fi";
+import { useState } from "react";
 
 export function Cadastro() {
   const navigate = useNavigate();
+
+  const [aluno, setAluno] = useState({
+    nome: "",
+    email: "",
+    cpf: "",
+    curso: "",
+    senha: "",
+  });
+
+  const onChangeAluno = (e) =>
+    setAluno({ ...aluno, [e.target.name]: e.target.value });
+
+  const Submit = (e) => {
+    e.preventDefault();
+    console.log(aluno);
+  };
+
   return (
     <Container>
-      <form>
+      <form onSubmit={Submit}>
         <h2>Faça seu cadastro</h2>
-        <input placeholder="nome"></input>
-        <input placeholder="insira seu email institucional"></input>
-        <input placeholder="cpf"></input>
-        <input type="password" placeholder="insira sua senha"></input>
+        <input
+          name="nome"
+          placeholder="nome"
+          value={aluno.nome}
+          onChange={onChangeAluno}
+        ></input>
+        <input
+          name="email"
+          placeholder="email institucional"
+          value={aluno.email}
+          onChange={onChangeAluno}
+        ></input>
+        <input
+          name="cpf"
+          placeholder="cpf"
+          value={aluno.cpf}
+          onChange={onChangeAluno}
+        ></input>
+        <input
+          name="curso"
+          placeholder="curso"
+          autoComplete="current-curso"
+          value={aluno.curso}
+          onChange={onChangeAluno}
+        ></input>
+        <input
+          type="password"
+          placeholder="insira sua senha"
+          value={aluno.senha}
+          onChange={onChangeAluno}
+        ></input>
+
         <button type="submit">Cadastrar</button>
-        {/* <a href="forget">Esqueci minha senha</a> */}
 
         <a href="/" onClick={() => navigate("/")}>
-          <FiLogIn />
+          <FiArrowLeft />
           Voltar para o login
         </a>
       </form>
