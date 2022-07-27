23d02c31fa19c488b01efed89384287eb8323443
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-07-27 18:11:10 -0300

    validando informações do cadastro

--- src/pages/CadastroPage/index.jsx
+++ src/pages/CadastroPage/index.jsx
@@ -2,6 +2,9 @@ import { Container } from "./styles";
 import { useNavigate } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import { useState } from "react";
+import * as Yup from "yup";
+
+
 
 export function Cadastro() {
   const navigate = useNavigate();
@@ -17,10 +20,42 @@ export function Cadastro() {
   const onChangeAluno = (e) =>
     setAluno({ ...aluno, [e.target.name]: e.target.value });
 
-  const Submit = (e) => {
+
+  const Submit = async (e) => {
+
+
     e.preventDefault();
-    console.log(aluno);
+   
+    let schema = Yup.object().shape({
+      nome: Yup.string().required("insira o seu nome"),
+      email: Yup.string().required("insira o seu email").email("email institucional"),
+      cpf: Yup.string().required("insira o seu cpf "),
+      curso: Yup.string().required("insira seu curso"),
+      senha: Yup.string().required("insira uma senha").min(6, "mínimo de 6 dígitos válidos"),
+    });
+    try {
+      await schema.validate(aluno, { abortEarly: false })
+
+
+      
+    } catch (err) {
+      const validationsErrors = {};
+
+      err.inner.forEach(error => {
+        validationsErrors[error.path] = error.message;
+      });
+
+      console.log(validationsErrors);
+    }
+
+
+
   };
+
+  // async function validate() {
+
+
+  // }
 
   return (
     <Container>
@@ -51,14 +86,17 @@ export function Cadastro() {
           value={aluno.curso}
           onChange={onChangeAluno}
         ></input>
+
         <input
           type="password"
+          id="pass"
+          name="password"
+          autoComplete="current-password"
           placeholder="insira sua senha"
-          value={aluno.senha}
-          onChange={onChangeAluno}
         ></input>
-
-        <button type="submit">Cadastrar</button>
+        <button type="submit" onClick={Submit}>
+          Cadastrar
+        </button>
 
         <a href="/" onClick={() => navigate("/")}>
           <FiArrowLeft />
